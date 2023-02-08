449836c7753b5b1b39bad4036fbef6f17293d58e
RafaelZielinski <rafekzielinski@wp.pl>
2023-02-09 00:38:59 +0100

    initialize first files

---
+++ src/main/java/com/example/functionalbackend/FunctionalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.functionalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionalBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/functionalbackend/FunctionalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.functionalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
