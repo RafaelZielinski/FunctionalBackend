8c131d1ef45de66700e120919dadbb12d915a8f5
RafaelZielinski <rafekzielinski@wp.pl>
2023-02-26 20:52:17 +0100

    add validation for request form

--- src/main/java/com/example/functionalbackend/security/auth/RegisterRequest.java
+++ src/main/java/com/example/functionalbackend/security/auth/RegisterRequest.java
@@ -1,5 +1,9 @@
 package com.example.functionalbackend.security.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +14,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+    @NotBlank
+    @Size(min = 5)
     private String firstname;
+    @NotBlank
+    @Size(min = 6)
     private String lastname;
+    @Email(message = "email should be valid")
     private String email;
+    @NotBlank
+    @Size(min = 6)
     private String password;
 }
