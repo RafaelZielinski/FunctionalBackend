b3f7d357abcd6a1fe937e9a00381d1d4ac1d0816
RafaelZielinski <rafekzielinski@wp.pl>
2023-02-28 04:50:43 +0100

    add handle exception in login and write integration tests on login

--- src/main/java/com/example/functionalbackend/security/service/AuthenticationService.java
+++ src/main/java/com/example/functionalbackend/security/service/AuthenticationService.java
@@ -3,10 +3,14 @@ package com.example.functionalbackend.security.service;
 import com.example.functionalbackend.security.auth.AuthenticationRequest;
 import com.example.functionalbackend.security.auth.AuthenticationResponse;
 import com.example.functionalbackend.security.auth.RegisterRequest;
+import com.example.functionalbackend.security.exception.UserAccountNotFoundException;
+import com.example.functionalbackend.security.exception.UserAlreadyExistsException;
 import com.example.functionalbackend.security.model.Role;
 import com.example.functionalbackend.security.model.User;
 import com.example.functionalbackend.security.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +18,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class AuthenticationService {
 
     private final UserRepository userRepository;
@@ -22,6 +27,12 @@ public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
+
+        if(userRepository.findByEmail(request.getEmail()).isPresent()) {
+            log.error("User already exists");
+            throw new UserAlreadyExistsException("User already exists");
+
+        }
         User user = User.builder()
                 .firstname(request.getFirstname())
                 .lastname(request.getLastname())
@@ -38,14 +49,18 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> {
+                    log.error("User does not exist");
+                    return new UserAccountNotFoundException("User does not exist");
+                });
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         request.getEmail(),
                         request.getPassword()
                 )
         );
-        User user = userRepository.findByEmail(request.getEmail())
-                .orElseThrow();
         String jwtToken = jwtService.generateToken(user);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
--- src/test/java/com/example/functionalbackend/security/controller/AuthenticationControllerIntegrationTest.java
+++ src/test/java/com/example/functionalbackend/security/controller/AuthenticationControllerIntegrationTest.java
@@ -1,16 +1,20 @@
 package com.example.functionalbackend.security.controller;
 
 import com.example.functionalbackend.FunctionalBackendApplication;
+import com.example.functionalbackend.security.model.Role;
+import com.example.functionalbackend.security.model.User;
 import com.example.functionalbackend.security.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -32,6 +36,13 @@ class AuthenticationControllerIntegrationTest {
     private UserRepository userRepository;
     @Autowired
     private ObjectMapper objectMapper;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+    }
 
     @Test
     void should_return_200_success_with_valid_credentials_to_registrate_and_token() throws Exception {
@@ -40,7 +51,6 @@ class AuthenticationControllerIntegrationTest {
                 "3cC5wbCIsImlhdCI6MTY3NzQyNjk5NiwiZXhwIjoxNjc3NDI4NDM" +
                 "2fQ.TUoeJe8kmxgwUbJ0QOOCxSNjkWpwr9rPcqznYf3bRKg";
         //when
-        userRepository.deleteAll();
         mockMvc.perform(post("/api/v1/auth/register")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(validUser().toString()))
@@ -53,16 +63,59 @@ class AuthenticationControllerIntegrationTest {
     }
 
     @Test
-    void should_return_403_forbidden_with_unvalid_credentials_to_registrate_and_token() throws Exception {
+    void should_return_400_bad_Request_with_unvalid_credentials_to_registrate_and_token() throws Exception {
         //given
         //when
         //then
-        userRepository.deleteAll();
         mockMvc.perform(post("/api/v1/auth/register")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(unvalidUser().toString()))
                 .andDo(print())
                 .andExpect(status().isBadRequest())
+                .andReturn();
+    }
+
+    @Test
+    void should_return_403_forbidden_with_present_account_in_database() throws Exception {
+        //given
+        userRepository.save(user());
+        //when
+        //then
+        mockMvc.perform(post("/api/v1/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(validUser().toString()))
+                .andDo(print())
+                .andExpect(status().isForbidden())
+                .andReturn();
+    }
+
+    @Test
+    void should_login_with_present_account_in_database() throws Exception {
+        //given
+        String expectedToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJyYWZla3ppZWxpbnNraUB" +
+                "3cC5wbCIsImlhdCI6MTY3NzQyNjk5NiwiZXhwIjoxNjc3NDI4NDM" +
+                "2fQ.TUoeJe8kmxgwUbJ0QOOCxSNjkWpwr9rPcqznYf3bRKg";
+        userRepository.save(user());
+        //when
+        //then
+        mockMvc.perform(post("/api/v1/auth/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(validUserLogin().toString()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    void should_no_logged_with_no_present_account_in_database() throws Exception {
+        //given
+        //when
+        //then
+        mockMvc.perform(post("/api/v1/auth/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(validUserLogin().toString()))
+                .andDo(print())
+                .andExpect(status().isForbidden())
                 .andReturn();
     }
 
@@ -83,6 +136,21 @@ class AuthenticationControllerIntegrationTest {
         jo.put("email", "rafekzielinski@wp.pl");
         jo.put("password", "password");
         return jo;
+    }
+    private JSONObject validUserLogin() throws JSONException {
+        JSONObject jo = new JSONObject();
+        jo.put("email", "rafekzielinski@wp.pl");
+        jo.put("password", "password");
+        return jo;
+    }
+    private User user() {
+        return User.builder()
+                .firstname("Rafał")
+                .lastname("Zieliński")
+                .email("rafekzielinski@wp.pl")
+                .password(passwordEncoder.encode("password"))
+                .role(Role.USER)
+                .build();
     }
 
     @Import(FunctionalBackendApplication.class)
